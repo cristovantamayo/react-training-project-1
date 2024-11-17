30cdd6abf14a7b2f5255d204b5adc0d7371e875b
Cristovan Tamayo <ctamayo@bionexo.com>
2024-11-17 10:04:46 -0300

    Cleaning Code

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,9 +1,8 @@
 export const PostCard = ({title, body, cover, id}) => (
     <div className="post">
         <img src={cover} alt={title} />
-        <div
-        className="post-content"> 
-        <h1>{title}</h1>
+        <div className="post-content"> 
+        <h2>{title}</h2>
         <p>{body}</p>
         </div>
     </div>
