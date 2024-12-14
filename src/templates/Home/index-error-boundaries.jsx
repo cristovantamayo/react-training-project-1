7c67f771e174c1cf925e5a3978195dc3d60bcb11
Cristovan Tamayo <ctamayo@bionexo.com>
2024-12-14 18:22:24 -0300

    First Compounded Component

---
+++ src/templates/Home/index-error-boundaries.jsx
@@ -0,0 +1,76 @@
+import React, { Component, useState, useEffect } from "react"; // Adjust the path as necessary
+
+const s = {
+  style: {
+    fontSize: "60px",
+    background: "green",
+  },
+};
+
+class MyErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  // eslint-disable-next-line
+  static getDerivedStateFromError(error) {
+    // Atualiza o state para que a próxima renderização mostre a UI alternativa.
+    return { hasError: true };
+  }
+
+  // eslint-disable-next-line
+  componentDidCatch(error, errorInfo) {
+    // Você também pode registrar o erro em um serviço de relatórios de erro
+    //logErrorToMyService(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Você pode renderizar qualquer UI alternativa
+      return <p {...s}>Algo deu errado.</p>;
+    }
+    // eslint-disable-next-line
+    return this.props.children;
+  }
+}
+
+const ItWillThrowError = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    if (counter > 3) {
+      throw new Error("Counter is greater than 3");
+    }
+  }, [counter]);
+
+  return (
+    <div>
+      <button onClick={() => setCounter((s) => s + 1)}>
+        Counter Up: {counter}
+      </button>
+    </div>
+  );
+};
+
+export const Home = () => {
+  return (
+    <div {...s}>
+      <MyErrorBoundary>
+        <ItWillThrowError />
+      </MyErrorBoundary>
+      <MyErrorBoundary>
+        <ItWillThrowError />
+      </MyErrorBoundary>
+      <MyErrorBoundary>
+        <ItWillThrowError />
+      </MyErrorBoundary>
+      <MyErrorBoundary>
+        <ItWillThrowError />
+      </MyErrorBoundary>
+      <MyErrorBoundary>
+        <ItWillThrowError />
+      </MyErrorBoundary>
+    </div>
+  );
+};
